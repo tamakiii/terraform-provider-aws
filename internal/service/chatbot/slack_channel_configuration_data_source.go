95b61dd02691dbc72eaed2a72c2f88ce09ab42e7
tamakiii <tamakiii@users.noreply.github.com>
2025-06-10 13:56:16 +0900

    fix and test

--- internal/service/chatbot/slack_channel_configuration_data_source.go
+++ internal/service/chatbot/slack_channel_configuration_data_source.go
@@ -3,59 +3,23 @@
 
 package chatbot
 
-// **PLEASE DELETE THIS AND ALL TIP COMMENTS BEFORE SUBMITTING A PR FOR REVIEW!**
-//
-// TIP: ==== INTRODUCTION ====
-// Thank you for trying the skaff tool!
-//
-// You have opted to include these helpful comments. They all include "TIP:"
-// to help you find and remove them when you're done with them.
-//
-// While some aspects of this file are customized to your input, the
-// scaffold tool does *not* look at the AWS API and ensure it has correct
-// function, structure, and variable names. It makes guesses based on
-// commonalities. You will need to make significant adjustments.
-//
-// In other words, as generated, this is a rough outline of the work you will
-// need to do. If something doesn't make sense for your situation, get rid of
-// it.
-
 import (
-	// TIP: ==== IMPORTS ====
-	// This is a common set of imports but not customized to your code since
-	// your code hasn't been written yet. Make sure you, your IDE, or
-	// goimports -w <file> fixes these imports.
-	//
-	// The provider linter wants your imports to be in two groups: first,
-	// standard library (i.e., "fmt" or "strings"), second, everything else.
-	//
-	// Also, AWS Go SDK v2 may handle nested structures differently than v1,
-	// using the services/chatbot/types package. If so, you'll
-	// need to import types and reference the nested types, e.g., as
-	// awstypes.<Type Name>.
 	"context"
 
+	"github.com/YakDriver/regexache"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/chatbot"
 	awstypes "github.com/aws/aws-sdk-go-v2/service/chatbot/types"
+	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/datasource"
 	"github.com/hashicorp/terraform-plugin-framework/datasource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-provider-aws/internal/create"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework"
 	"github.com/hashicorp/terraform-provider-aws/internal/framework/flex"
 	"github.com/hashicorp/terraform-provider-aws/names"
 )
-
-// TIP: ==== FILE STRUCTURE ====
-// All data sources should follow this basic outline. Improve this data source's
-// maintainability by sticking to it.
-//
-// 1. Package declaration
-// 2. Imports
-// 3. Main data source struct with schema method
-// 4. Read method
-// 5. Other functions (flatteners, expanders, waiters, finders, etc.)
 
 // Function annotations are used for datasource registration to the Provider. DO NOT EDIT.
 // @FrameworkDataSource("aws_chatbot_slack_channel_configuration", name="Slack Channel Configuration")
@@ -71,63 +35,79 @@ type dataSourceSlackChannelConfiguration struct {
 	framework.DataSourceWithConfigure
 }
 
-// TIP: ==== SCHEMA ====
-// In the schema, add each of the arguments and attributes in snake
-// case (e.g., delete_automated_backups).
-// * Alphabetize arguments to make them easier to find.
-// * Do not add a blank line between arguments/attributes.
-//
-// Users can configure argument values while attribute values cannot be
-// configured and are used as output. Arguments have either:
-// Required: true,
-// Optional: true,
-//
-// All attributes will be computed and some arguments. If users will
-// want to read updated information or detect drift for an argument,
-// it should be computed:
-// Computed: true,
-//
-// You will typically find arguments in the input struct
-// (e.g., CreateDBInstanceInput) for the create operation. Sometimes
-// they are only in the input struct (e.g., ModifyDBInstanceInput) for
-// the modify operation.
-//
-// For more about schema options, visit
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/schemas?page=schemas
 func (d *dataSourceSlackChannelConfiguration) Schema(ctx context.Context, req datasource.SchemaRequest, resp *datasource.SchemaResponse) {
 	resp.Schema = schema.Schema{
+		Description: "Data source for managing an AWS Chatbot Slack Channel Configuration.",
 		Attributes: map[string]schema.Attribute{
 			"chat_configuration_arn": schema.StringAttribute{
-				Required: true,
+				Description: "ARN of the Slack channel configuration.",
+				Required:    true,
+				Validators: []validator.String{
+					stringvalidator.RegexMatches(
+						regexache.MustCompile(`^arn:aws[0-9a-zA-Z-]*:chatbot::[0-9]{12}:chat-configuration/slack-channel/.+$`),
+						"must be a valid AWS Chatbot Slack channel configuration ARN",
+					),
+				},
+			},
+			"configuration_name": schema.StringAttribute{
+				Description: "Name of the Slack channel configuration.",
+				Computed:    true,
+			},
+			"iam_role_arn": schema.StringAttribute{
+				Description: "ARN of the IAM role that defines the permissions for AWS Chatbot.",
+				Computed:    true,
+			},
+			"logging_level": schema.StringAttribute{
+				Description: "Specifies the logging level for this configuration.",
+				Computed:    true,
+			},
+			"slack_channel_id": schema.StringAttribute{
+				Description: "ID of the Slack channel.",
+				Computed:    true,
+			},
+			"slack_channel_name": schema.StringAttribute{
+				Description: "Name of the Slack channel.",
+				Computed:    true,
+			},
+			"slack_team_id": schema.StringAttribute{
+				Description: "ID of the Slack workspace authorized with AWS Chatbot.",
+				Computed:    true,
+			},
+			"slack_team_name": schema.StringAttribute{
+				Description: "Name of the Slack workspace.",
+				Computed:    true,
+			},
+			"sns_topic_arns": schema.SetAttribute{
+				Description: "ARNs of the SNS topics that deliver notifications to AWS Chatbot.",
+				ElementType: types.StringType,
+				Computed:    true,
+			},
+			"state": schema.StringAttribute{
+				Description: "State of the configuration.",
+				Computed:    true,
+			},
+			"tags": schema.MapAttribute{
+				Description: "Map of tags assigned to the resource.",
+				ElementType: types.StringType,
+				Computed:    true,
+			},
+			"user_authorization_required": schema.BoolAttribute{
+				Description: "Enables use of a user role requirement in your chat configuration.",
+				Computed:    true,
 			},
 		},
 	}
 }
 
-// TIP: ==== ASSIGN CRUD METHODS ====
-// Data sources only have a read method.
 func (d *dataSourceSlackChannelConfiguration) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
-	// TIP: ==== DATA SOURCE READ ====
-	// Generally, the Read function should do the following things. Make
-	// sure there is a good reason if you don't do one of these.
-	//
-	// 1. Get a client connection to the relevant service
-	// 2. Fetch the config
-	// 3. Get information about a resource from AWS
-	// 4. Set the ID, arguments, and attributes
-	// 5. Set the tags
-	// 6. Set the state
-	// TIP: -- 1. Get a client connection to the relevant service
 	conn := d.Meta().ChatbotClient(ctx)
 
-	// TIP: -- 2. Fetch the config
 	var data dataSourceSlackChannelConfigurationModel
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 3. Get information about a resource from AWS
 	out, err := findSlackChannelConfigurationByArn(ctx, conn, data.ChatConfigurationArn.ValueString())
 	if err != nil {
 		resp.Diagnostics.AddError(
@@ -137,73 +117,61 @@ func (d *dataSourceSlackChannelConfiguration) Read(ctx context.Context, req data
 		return
 	}
 
-	// TIP: -- 4. Set the ID, arguments, and attributes
 	// Using a field name prefix allows mapping fields such as `SlackChannelConfigurationId` to `ID`
 	resp.Diagnostics.Append(flex.Flatten(ctx, out, &data, flex.WithFieldNamePrefix("SlackChannelConfiguration"))...)
 	if resp.Diagnostics.HasError() {
 		return
 	}
 
-	// TIP: -- 5. Set the tags
 	data.ChatConfigurationArn = flex.StringToFramework(ctx, out.ChatConfigurationArn)
 
-	// TIP: -- 6. Set the state
+	// Set tags if present
+	if len(out.Tags) > 0 {
+		data.Tags = flex.FlattenFrameworkStringValueMap(ctx, chatbotTagsToMap(out.Tags))
+	}
+
 	resp.Diagnostics.Append(resp.State.Set(ctx, &data)...)
 }
 
-func findSlackChannelConfigurationByArn(ctx context.Context, conn *chatbot.Client, chat_configuration_arn string) (*awstypes.SlackChannelConfiguration, error) {
-	input := &chatbot.DescribeSlackChannelConfigurationsInput{
-		ChatConfigurationArn: aws.String(chat_configuration_arn),
-	}
+func findSlackChannelConfigurationByArn(ctx context.Context, conn *chatbot.Client, chatConfigurationArn string) (*awstypes.SlackChannelConfiguration, error) {
+	input := &chatbot.DescribeSlackChannelConfigurationsInput{}
 
-	for {
-		output, error := conn.DescribeSlackChannelConfigurations(ctx, input)
-		if error != nil {
-			return nil, error
+	pages := chatbot.NewDescribeSlackChannelConfigurationsPaginator(conn, input)
+	for pages.HasMorePages() {
+		page, err := pages.NextPage(ctx)
+		if err != nil {
+			return nil, err
 		}
 
-		for _, configuration := range output.SlackChannelConfigurations {
-			if aws.ToString(configuration.ChatConfigurationArn) == chat_configuration_arn {
+		for _, configuration := range page.SlackChannelConfigurations {
+			if aws.ToString(configuration.ChatConfigurationArn) == chatConfigurationArn {
 				return &configuration, nil
 			}
 		}
-
-		if output.NextToken == nil {
-			break
-		}
-
-		input.NextToken = output.NextToken
 	}
 
-	// If we are here, then we need to return an error that the configuration was not found.
-	return nil, create.Error(names.Chatbot, "missing", DSNameSlackChannelConfiguration, chat_configuration_arn, nil)
+	return nil, create.Error(names.Chatbot, create.ErrActionReading, DSNameSlackChannelConfiguration, chatConfigurationArn, nil)
 }
 
-// TIP: ==== DATA STRUCTURES ====
-// With Terraform Plugin-Framework configurations are deserialized into
-// Go types, providing type safety without the need for type assertions.
-// These structs should match the schema definition exactly, and the `tfsdk`
-// tag value should match the attribute name.
-//
-// Nested objects are represented in their own data struct. These will
-// also have a corresponding attribute type mapping for use inside flex
-// functions.
-//
-// See more:
-// https://developer.hashicorp.com/terraform/plugin/framework/handling-data/accessing-values
+func chatbotTagsToMap(tags []awstypes.Tag) map[string]string {
+	m := make(map[string]string, len(tags))
+	for _, tag := range tags {
+		m[aws.ToString(tag.TagKey)] = aws.ToString(tag.TagValue)
+	}
+	return m
+}
+
 type dataSourceSlackChannelConfigurationModel struct {
 	ChatConfigurationArn      types.String `tfsdk:"chat_configuration_arn"`
-	SlackTeamName             types.String `tfsdk:"slack_team_name"`
+	ConfigurationName         types.String `tfsdk:"configuration_name"`
+	IamRoleArn                types.String `tfsdk:"iam_role_arn"`
+	LoggingLevel              types.String `tfsdk:"logging_level"`
 	SlackChannelId            types.String `tfsdk:"slack_channel_id"`
 	SlackChannelName          types.String `tfsdk:"slack_channel_name"`
-	IamRoleArn                types.String `tfsdk:"iam_role_arn"`
-	ConfigurationName         types.String `tfsdk:"configuration_name"`
-	LoggingLevel              types.String `tfsdk:"logging_level"`
-	UserAuthorizationRequired types.Bool   `tfsdk:"user_authorization_required"`
+	SlackTeamId               types.String `tfsdk:"slack_team_id"`
+	SlackTeamName             types.String `tfsdk:"slack_team_name"`
+	SnsTopicArns              types.Set    `tfsdk:"sns_topic_arns"`
 	State                     types.String `tfsdk:"state"`
-}
-
-type complexArgumentModel struct {
-	NestedRequired types.String `tfsdk:"nested_required"`
-	NestedOptional types.String `tfsdk:"nested_optional"`
+	Tags                      types.Map    `tfsdk:"tags"`
+	UserAuthorizationRequired types.Bool   `tfsdk:"user_authorization_required"`
 }
