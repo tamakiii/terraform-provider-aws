e84e34e1efdadb034031ad57b2a6b465f9b2e50c
tamakiii <tamakiii@users.noreply.github.com>
2025-06-10 15:42:15 +0900

    unit test improvement

--- internal/service/chatbot/slack_channel_configuration_data_source_test.go
+++ internal/service/chatbot/slack_channel_configuration_data_source_test.go
@@ -20,6 +20,11 @@ func TestAccChatbotSlackChannelConfigurationDataSource_basic(t *testing.T) {
 	dataSourceName := "data.aws_chatbot_slack_channel_configuration.test"
 	resourceName := "aws_chatbot_slack_channel_configuration.test"
 
+	// The slack workspace must be created via the AWS Console. It cannot be created via APIs or Terraform.
+	// Once it is created, export the workspace details in the env variables for this test
+	teamID := acctest.SkipIfEnvVarNotSet(t, envSlackTeamID)
+	channelID := acctest.SkipIfEnvVarNotSet(t, envSlackChannelID)
+
 	resource.ParallelTest(t, resource.TestCase{
 		PreCheck: func() {
 			acctest.PreCheck(ctx, t)
@@ -31,7 +36,7 @@ func TestAccChatbotSlackChannelConfigurationDataSource_basic(t *testing.T) {
 		CheckDestroy:             testAccCheckSlackChannelConfigurationDestroy(ctx),
 		Steps: []resource.TestStep{
 			{
-				Config: testAccSlackChannelConfigurationDataSourceConfig_basic(rName),
+				Config: testAccSlackChannelConfigurationDataSourceConfig_basic(rName, channelID, teamID),
 				Check: resource.ComposeAggregateTestCheckFunc(
 					resource.TestCheckResourceAttrPair(dataSourceName, "chat_configuration_arn", resourceName, "chat_configuration_arn"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "configuration_name", resourceName, "configuration_name"),
@@ -42,7 +47,44 @@ func TestAccChatbotSlackChannelConfigurationDataSource_basic(t *testing.T) {
 					resource.TestCheckResourceAttrPair(dataSourceName, "slack_team_id", resourceName, "slack_team_id"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "slack_team_name", resourceName, "slack_team_name"),
 					resource.TestCheckResourceAttrPair(dataSourceName, "user_authorization_required", resourceName, "user_authorization_required"),
-					acctest.MatchResourceAttrRegionalARN(ctx, dataSourceName, "chat_configuration_arn", "chatbot", regexache.MustCompile(`chat-configuration/slack-channel/.+$`)),
+					resource.TestCheckResourceAttrPair(dataSourceName, "state", resourceName, "state"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "sns_topic_arns", resourceName, "sns_topic_arns"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "tags", resourceName, "tags"),
+					// Use GlobalARN matcher as Chatbot ARNs are global resources
+					acctest.MatchResourceAttrGlobalARN(ctx, dataSourceName, "chat_configuration_arn", "chatbot", regexache.MustCompile(fmt.Sprintf(`chat-configuration/slack-channel/%s$`, rName))),
+				),
+			},
+		},
+	})
+}
+
+func TestAccChatbotSlackChannelConfigurationDataSource_tags(t *testing.T) {
+	ctx := acctest.Context(t)
+	rName := sdkacctest.RandomWithPrefix(acctest.ResourcePrefix)
+	dataSourceName := "data.aws_chatbot_slack_channel_configuration.test"
+	resourceName := "aws_chatbot_slack_channel_configuration.test"
+
+	teamID := acctest.SkipIfEnvVarNotSet(t, envSlackTeamID)
+	channelID := acctest.SkipIfEnvVarNotSet(t, envSlackChannelID)
+
+	resource.ParallelTest(t, resource.TestCase{
+		PreCheck: func() {
+			acctest.PreCheck(ctx, t)
+			acctest.PreCheckPartitionHasService(t, names.Chatbot)
+			testAccPreCheck(ctx, t)
+		},
+		ErrorCheck:               acctest.ErrorCheck(t, names.ChatbotServiceID),
+		ProtoV5ProviderFactories: acctest.ProtoV5ProviderFactories,
+		CheckDestroy:             testAccCheckSlackChannelConfigurationDestroy(ctx),
+		Steps: []resource.TestStep{
+			{
+				Config: testAccSlackChannelConfigurationDataSourceConfig_tags(rName, channelID, teamID),
+				Check: resource.ComposeAggregateTestCheckFunc(
+					resource.TestCheckResourceAttrPair(dataSourceName, "chat_configuration_arn", resourceName, "chat_configuration_arn"),
+					resource.TestCheckResourceAttrPair(dataSourceName, "tags", resourceName, "tags"),
+					resource.TestCheckResourceAttr(dataSourceName, "tags.%", "2"),
+					resource.TestCheckResourceAttr(dataSourceName, "tags.key1", "value1"),
+					resource.TestCheckResourceAttr(dataSourceName, "tags.key2", "value2"),
 				),
 			},
 		},
@@ -62,24 +104,32 @@ func TestAccChatbotSlackChannelConfigurationDataSource_notFound(t *testing.T) {
 		Steps: []resource.TestStep{
 			{
 				Config:      testAccSlackChannelConfigurationDataSourceConfig_notFound(),
-				ExpectError: regexache.MustCompile(`missing`),
+				ExpectError: regexache.MustCompile(`empty result`),
 			},
 		},
 	})
 }
 
-func testAccSlackChannelConfigurationDataSourceConfig_basic(rName string) string {
-	return acctest.ConfigCompose(testAccSlackChannelConfigurationConfig_basic(rName, "C07EZ1NHXEZ", "T07EA7JMZ"), fmt.Sprintf(`
+func testAccSlackChannelConfigurationDataSourceConfig_basic(rName, channelID, teamID string) string {
+	return acctest.ConfigCompose(testAccSlackChannelConfigurationConfig_basic(rName, channelID, teamID), `
 data "aws_chatbot_slack_channel_configuration" "test" {
   chat_configuration_arn = aws_chatbot_slack_channel_configuration.test.chat_configuration_arn
 }
-`))
+`)
+}
+
+func testAccSlackChannelConfigurationDataSourceConfig_tags(rName, channelID, teamID string) string {
+	return acctest.ConfigCompose(testAccSlackChannelConfigurationConfig_tags1(rName, channelID, teamID, "key1", "value1"), `
+data "aws_chatbot_slack_channel_configuration" "test" {
+  chat_configuration_arn = aws_chatbot_slack_channel_configuration.test.chat_configuration_arn
+}
+`)
 }
 
 func testAccSlackChannelConfigurationDataSourceConfig_notFound() string {
 	return `
 data "aws_chatbot_slack_channel_configuration" "test" {
-  chat_configuration_arn = "arn:aws:chatbot::123456789012:chat-configuration/slack-channel/non-existent"
+  chat_configuration_arn = "arn:aws:chatbot::123456789012:chat-configuration/slack-channel/does-not-exist"
 }
 `
 }
